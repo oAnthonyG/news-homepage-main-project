b5f4bfe8361c2bbc77167bf14ec6623bd0d9e6cd
Anthony <anthonytguilherme@hotmail.com>
2026-03-30 16:32:27 -0300

    fix: adiciona theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { ChakraProvider } from "@chakra-ui/react"
 import App from "./App"
-import system from "./components/Theme/theme"
+import system from "./components/Theme/Theme"
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
